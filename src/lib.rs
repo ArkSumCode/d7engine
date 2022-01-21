123012ceb25304cbe4668eec7e1e77edece8e080
Markus Dick <36760856+debugseven@users.noreply.github.com>
2022-01-21 17:19:55 +0100

    update performace -> delta

--- src/lib.rs
+++ src/lib.rs
@@ -161,7 +161,7 @@ pub fn init(config: project::Config, runtime: &mut impl project::Runtime) {
         window.gl_swap_window();
 
         // performance tick
-        let _delta = performance.frame();
+        performance.frame();
     }
 
 }
@@ -184,6 +184,7 @@ it holds the timestamp of the last frame and the current fps
 pub struct Performance {
     last_frame: Instant,
     fps: f32, 
+    delta: f32,
 }
 
 impl Performance {
@@ -191,20 +192,26 @@ impl Performance {
     pub fn new() -> Performance {
         let last_frame = Instant::now();
         let fps = 0.0;
-        Performance { last_frame, fps }
+        let delta = 0.0;
+        Performance { last_frame, fps, delta }
     }
 
     // calculates fps and returns the delta time
-    pub fn frame(&mut self) -> f32 {
+    pub fn frame(&mut self) {
         let elapsed = self.last_frame.elapsed();
         self.last_frame =  Instant::now();
         self.fps = (1_000_000_000 / elapsed.as_nanos()) as f32;
-        1.0 / self.fps
+        self.delta = 1.0 / self.fps;
     }
 
     // returns the current fps
     pub fn fps(&self) -> f32 {
         self.fps
+    }
+
+    // returns the current delta
+    pub fn delta(&self) -> f32 {
+        self.delta
     }
 }
 
