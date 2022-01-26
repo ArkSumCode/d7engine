6a1636fdc0bf9efa52cbe73fba120764d094f9be
Markus Dick <36760856+debugseven@users.noreply.github.com>
2022-01-26 23:12:40 +0100

    change filesystem installation

--- src/core/file.rs
+++ src/core/file.rs
@@ -8,42 +8,61 @@ files system only works with an installation struct, so
 it is save that the %appdata%/d7engine/<title> folders exist
 */
 pub struct Installation {
-    path: String
+    title: String,
+    path: Option<String>,
 }
 
 impl Installation {
+    // create a new empty Installation
+    // we put the files in a subfolder of d7engine
+    // with the name title
+    pub fn new(title: String) -> Installation {
+        Installation {title, path: None}
+    }
+
     /*
     creates a folder for the game in appdata
     call this method first if the game needs some file storage on a user pc
     */
-    pub fn new(title: &str) -> Result<Installation, String> {
+    pub fn create_folder(&mut self) -> Result<(), String> {
         let path = appdata()?;
         let mut path = PathBuf::from(&path);
         
         // create folders %appdata%\d7engine\<title>
         update_folder(&mut path, "d7engine")?;
-        update_folder(&mut path, title)?;
+        update_folder(&mut path, &self.title)?;
 
         let path = path_as_string(path.as_path())?;
-        Ok(Installation{path: path})
+        self.path = Some(path);
+        Ok(())
     }
 
     // create or overwrite a file in %appdata%/d7engine/<title>
-    pub fn overwrite(&self, file: &str, text: &str) -> Result<(), String> {
-        // create pathbuffer and add the file that
-        let mut path = PathBuf::from(&self.path);
-        path.push(file);
-        let path = path_as_string(path.as_path())?;
+    pub fn overwrite(&mut self, file: &str, text: &str) -> Result<(), String> {
+        // if the folder is not created
+        if let None = &self.path {
+            // create it now
+            self.create_folder()?;
+        }
 
-        // create the filestream
-        if let Ok(mut open) = File::create(&path) {
-            // write to the filestream
-            if let Ok(_) = open.write_all(text.as_bytes()) {
-                return Ok(())
-            }
-        } 
+        if let Some(path) = &self.path {
+            // create pathbuffer and add the file that
+            let mut path = PathBuf::from(&path);
+            path.push(file);
+            let path = path_as_string(path.as_path())?;
 
-        Err(format!("could not write file {}.", file))
+            // create the filestream
+            if let Ok(mut open) = File::create(&path) {
+                // write to the filestream
+                if let Ok(_) = open.write_all(text.as_bytes()) {
+                    return Ok(())
+                }
+            } 
+
+            return Err(format!("could not write file {}.", file));
+        }
+
+        Err("could not create folder".to_string())
     }
 }
 
