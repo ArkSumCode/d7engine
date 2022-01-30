145fa1c3b8d2bc65e251a16ad2984be24531d460
Markus Dick <36760856+debugseven@users.noreply.github.com>
2022-01-30 19:02:52 +0100

    add width and height to resource::Image

--- src/core/resource/image.rs
+++ src/core/resource/image.rs
@@ -7,12 +7,19 @@ use image;
 
 pub struct Image {
     data: Box<image::RgbaImage>,
+    width: u32,
+    height: u32,
 }
 
 impl Image {
     pub fn new(path: &str) -> Result<Image, String> {
         let data = Image::data(path)?; 
-        Ok(Image{data: Box::new(data)})
+        let width = data.width();
+        let height = data.height();
+        Ok(Image{
+            data: Box::new(data),
+            width, height
+        })
     }
 
     // convert the image to an image::RgbaImage which can be used 
@@ -36,10 +43,23 @@ impl Image {
     }
 
     // crop an image out of this image
-    pub fn crop(&self, x: i32, y: i32, width: i32, height: i32) -> Image {
+    pub fn crop(&self, x: u32, y: u32, width: u32, height: u32) -> Image {
         let mut data = self.to_rgba_image();
-        let img = image::imageops::crop(&mut data, x as u32, y as u32, width as u32, height as u32);
-        Image {data: Box::new(img.to_image())}
+        let img = image::imageops::crop(&mut data, x, y, width, height);
+        Image{
+            data: Box::new(img.to_image()),
+            width, height,
+        }
+    }
+
+    // returns the width of the image
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    // returns the height of the image
+    pub fn height(&self) -> u32 {
+        self.height
     }
 }
 
--- src/shader/rect.frag
+++ src/shader/rect.frag
@@ -8,5 +8,5 @@ out vec4 Color;
 
 void main()
 {
-    Color = vec4(1.0, 1.0, 0.0, 1.0);
+    Color = vec4(IN.Color, 1.0);
 }
